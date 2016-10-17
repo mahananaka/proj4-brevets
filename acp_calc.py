5748e0f3adaa5e6f36c4bdc132afcc0c97bfa509
mahananaka <jpaeschk@uoregon.edu>
2016-10-16 20:40:00 -0700

    changed tests

--- acp_calc.py
+++ acp_calc.py
@@ -68,7 +68,8 @@ class AcpBrevet():
 			if(remaining_dist>0):
 				dist_seg = min(remaining_dist,span)
 				print("dist_seg: {}".format(dist_seg))
-				hrs = int(hrs + dist_seg / speed)
+				#hrs = int(hrs + dist_seg / speed)
+				hrs = dist_seg / speed
 				mins = dist_seg % speed
 				#mins = mins + int(round((dist_seg % speed)*MINUTES_PER_HOUR))
 				remaining_dist = remaining_dist - span
