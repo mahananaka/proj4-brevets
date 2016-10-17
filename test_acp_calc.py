8449aa58041eeb7e0f044ef8eaa486b18ccc6d12
mahananaka <jpaeschk@uoregon.edu>
2016-10-16 21:31:47 -0700

    Moding test functions

--- test_acp_calc.py
+++ test_acp_calc.py
@@ -18,7 +18,7 @@ def test_new_brevet():
     brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
     assert brevet.brev_length == 1000
     assert brevet.brev_start == arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_control_times() == [{'description':'','distance': 0, "open":brevet.brev_start, "close":brevet.brev_start.replace(hours=+1)}]
+    assert brevet.get_control_times() == [{"description":"","distance": 0, "open":brevet.brev_start, "close":brevet.brev_start.replace(hours=+1)}]
 
 def test_add_control():
 	"""
@@ -32,7 +32,9 @@ def test_add_control():
 	cp = brevet.make_controle_point(350,"")
 	brevet.add_controle_point(cp)
 
-	assert brevet.get_control_times() == [{'description':'','distance': 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
-										  {'description':'','distance': 0, "open":arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')},
-										  {'description':'','distance': 0, "open":arrow.get('2016-11-10T13:51:00+00:00'), "close":arrow.get('2016-11-10T21:16:00+00:00')},
-										  {'description':'','distance': 0, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
+	array_compare = [{'description':'','distance': 0, 'open':arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+					 {'description':'','distance': 0, 'open':arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')},
+					 {'description':'','distance': 0, 'open':arrow.get('2016-11-10T13:51:00+00:00'), "close":arrow.get('2016-11-10T21:16:00+00:00')},
+					 {'description':'','distance': 0, 'open':arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
+
+	assert brevet.get_control_times() == array_compare
