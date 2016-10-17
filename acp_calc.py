cc0bde5899e56f3d7ecc9e2306db0c16eda38892
mahananaka <jpaeschk@uoregon.edu>
2016-10-16 20:58:35 -0700

    changed tests

--- acp_calc.py
+++ acp_calc.py
@@ -69,7 +69,7 @@ class AcpBrevet():
 				dist_seg = min(remaining_dist,span)
 				print("dist_seg: {}".format(dist_seg))
 				hrs = int(hrs + dist_seg / speed)
-				print("minutes to add: {}".formt(int(round((dist_seg/speed - hrs)*MINUTES_PER_HOUR))))
+				print("minutes to add: {}".format(int(round((dist_seg/speed - hrs)*MINUTES_PER_HOUR))))
 				mins = mins + int(round((dist_seg/speed - hrs)*MINUTES_PER_HOUR))
 				remaining_dist = remaining_dist - span
 				print("r_dist: {}\nhrs: {}\nmins: {}\nspan: {}\nspeed: {}\n\n".format(remaining_dist,hrs,mins,span,speed))
