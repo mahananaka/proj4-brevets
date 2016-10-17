a18d4bfdbaab9a4350ba6f02aaf37b046c0f0ff6
mahananaka <jpaeschk@uoregon.edu>
2016-10-17 01:59:44 -0700

    Added new test scenario

--- test_acp_calc.py
+++ test_acp_calc.py
@@ -29,34 +29,39 @@ def test_add_control():
 	brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
 	cp = brevet.make_controle_point(100,"First Checkpoint")
 	brevet.add_controle_point(cp)
-	cp = brevet.make_controle_point(199,"2nd")
-	brevet.add_controle_point(cp)
 	cp = brevet.make_controle_point(350,"")
 	brevet.add_controle_point(cp)
+	cp = brevet.make_controle_point(890,"3rd")
+	brevet.add_controle_point(cp)
 
-	should_be = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+	expected_output = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
 					 {"description":"First Checkpoint","distance": 100, "open":arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')},
-					 {"description":"2nd","distance": 199, "open":arrow.get('2016-11-10T13:51:00+00:00'), "close":arrow.get('2016-11-10T21:16:00+00:00')},
-					 {"description":"","distance": 350, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
+					 {"description":"","distance": 350, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')},
+					 {"description":"3rd","distance": 890, "open":arrow.get('2016-11-11T13:09:00+00:00'), "close":arrow.get('2016-11-13T01:23:00+00:00')},]
 
-	assert brevet.get_control_times() == should_be
+	assert brevet.get_control_times() == expected_output
 
 def test_update_control_point():
-
+	"""
+	This tests the update_control_point method. First assert is testing a baseline.
+	"""
 	brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
 	cp = brevet.make_controle_point(100,"First Checkpoint")
 	brevet.add_controle_point(cp)
 
-	should_be = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+	expected_output = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
 				 {"description":"First Checkpoint","distance": 100, "open":arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')}]
 
-	assert brevet.get_control_times() == should_be
+	assert brevet.get_control_times() == expected_output
 
+	"""
+	Next we actually test the update method
+	"""
 	brevet.update_controle_point(1,0,"Start Line")
 	brevet.update_controle_point(2,350,"1st Checkpoint")
 
-	should_be = [{"description":"Start Line","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+	expected_output = [{"description":"Start Line","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
 				 {"description":"1st Checkpoint","distance": 350, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
 
-	assert brevet.get_control_times() == should_be
+	assert brevet.get_control_times() == expected_output
 
