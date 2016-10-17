23134eae7bb6c24dbbca20bf050feb097acd185e
mahananaka <jpaeschk@uoregon.edu>
2016-10-16 21:54:09 -0700

    Moding test functions

--- test_acp_calc.py
+++ test_acp_calc.py
@@ -24,7 +24,7 @@ def test_new_brevet():
 
 def test_add_control():
 	"""
-	Adding a few new control points
+	Testing adding a few new control points
 	"""
 	brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
 	cp = brevet.make_controle_point(100,"First Checkpoint")
@@ -34,9 +34,29 @@ def test_add_control():
 	cp = brevet.make_controle_point(350,"")
 	brevet.add_controle_point(cp)
 
-	array_compare = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+	should_be = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
 					 {"description":"First Checkpoint","distance": 100, "open":arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')},
 					 {"description":"2nd","distance": 199, "open":arrow.get('2016-11-10T13:51:00+00:00'), "close":arrow.get('2016-11-10T21:16:00+00:00')},
 					 {"description":"","distance": 350, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
 
-	assert brevet.get_control_times() == array_compare
+	assert brevet.get_control_times() == should_be
+
+def test_update_control_point():
+
+	brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
+	cp = brevet.make_controle_point(100,"First Checkpoint")
+	brevet.add_controle_point(cp)
+
+	should_be = [{"description":"","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+				 {"description":"First Checkpoint","distance": 100, "open":arrow.get('2016-11-10T10:56:00+00:00'), "close":arrow.get('2016-11-10T14:40:00+00:00')}]
+
+	assert brevet.get_control_times() == should_be
+
+	brevet.update_controle_point(0,0,"Start Line")
+	brevet.update_controle_point(1,350,"1st Checkpoint")
+
+	should_be = [{"description":"Start Line","distance": 0, "open":arrow.get('2016-11-10T08:00:00+00:00'), "close":arrow.get('2016-11-10T09:00:00+00:00')},
+				 {"description":"1st Checkpoint","distance": 350, "open":arrow.get('2016-11-10T18:34:00+00:00'), "close":arrow.get('2016-11-11T07:20:00+00:00')}]
+
+	assert brevet.get_control_times() == should_be
+
