38ac15ca80fc2cc08995242f8a12ff4266b2f07f
mahananaka <jpaeschk@uoregon.edu>
2016-10-16 20:08:51 -0700

    changed tests

--- test_acp_calc.py
+++ test_acp_calc.py
@@ -17,6 +17,20 @@ def test_new_brevet():
     """
     brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
     assert brevet.brev_length == 1000
-
     assert brevet.brev_start == arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm')
     assert brevet.get_control_times() == [{'description':'','distance': 0, "open":brevet.brev_start, "close":brevet.brev_start.replace(hours=+1)}]
+
+def test_add_control():
+	"""
+	Adding a few new control points
+	"""
+	brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
+	cp = brevet.make_controle_point(100,"First Checkpoint")
+	brevet.add_controle_point(cp)
+	cp = brevet.make_controle_point(199,"2nd")
+	brevet.add_controle_point(cp)
+	cp = brevet.make_controle_point(350,"")
+	brevet.add_controle_point(cp)
+
+	assert brevet.get_control_times() == [{'description':'','distance': 0, "open":brevet.brev_start, "close":brevet.brev_start.replace(hours=+1)}]
+
