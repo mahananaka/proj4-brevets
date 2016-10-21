a05a24c52fe7e9650172ad351b77529f47e55d3c
mahananaka <jpaeschk@uoregon.edu>
2016-10-20 22:55:56 -0700

    altered methods of calc and testing

--- test_acp_calc.py
+++ test_acp_calc.py
@@ -19,17 +19,6 @@ def test_new_brevet():
     assert brevet.brev_length == 1000
     assert brevet.brev_start == arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm')
 
-def test_brevet_sets():
-    """
-    Testing initializing AcpBrevet
-    """
-    brevet = AcpBrevet( 1000, arrow.get('2016-11-10 08:00', 'YYYY-MM-DD HH:mm') )
-    brevet.set_brevet_length(200)
-    brevet.set_brevet_start(arrow.get('2016-11-10 10:00', 'YYYY-MM-DD HH:mm') )
-
-    assert brevet.brev_length == 200
-    assert brevet.brev_start == arrow.get('2016-11-10 10:00', 'YYYY-MM-DD HH:mm')
-
 def test_brevet_example1():
     """
     Testing initializing AcpBrevet
@@ -37,13 +26,13 @@ def test_brevet_example1():
     brevet = AcpBrevet( 200, arrow.get('2016-11-10 10:00', 'YYYY-MM-DD HH:mm') )
     
     #open
-    assert brevet.get_open(60, brevet.brev_length) == arrow.get('2016-11-10 11:46', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(120, brevet.brev_length) == arrow.get('2016-11-10 13:32', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(175, brevet.brev_length) == arrow.get('2016-11-10 15:09', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(200, brevet.brev_length) == arrow.get('2016-11-10 15:53', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_open(60, brevet.brev_length) == arrow.get('2016-11-10 11:46', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_open(120, brevet.brev_length) == arrow.get('2016-11-10 13:32', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_open(175, brevet.brev_length) == arrow.get('2016-11-10 15:09', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_open(200, brevet.brev_length) == arrow.get('2016-11-10 15:53', 'YYYY-MM-DD HH:mm')
 
     #closing
-    assert brevet.get_open(60, brevet.brev_length) == arrow.get('2016-11-10 14:00', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(120, brevet.brev_length) == arrow.get('2016-11-10 18:00', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(175, brevet.brev_length) == arrow.get('2016-11-10 11:40', 'YYYY-MM-DD HH:mm')
-    assert brevet.get_open(200, brevet.brev_length) == arrow.get('2016-11-10 13:30', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_close(60, brevet.brev_length) == arrow.get('2016-11-10 14:00', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_close(120, brevet.brev_length) == arrow.get('2016-11-10 18:00', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_close(175, brevet.brev_length) == arrow.get('2016-11-10 11:40', 'YYYY-MM-DD HH:mm')
+    assert brevet.calc_close(200, brevet.brev_length) == arrow.get('2016-11-10 13:30', 'YYYY-MM-DD HH:mm')
